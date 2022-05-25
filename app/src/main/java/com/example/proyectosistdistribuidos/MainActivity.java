3cc5caae7d0bafedf433eae0b4b9fc8fc33b1126
Sergio <sergio.gabriel.oficial@gmail.com>
2022-05-24 22:41:24 -0400

    Prueba

--- app/src/main/java/com/example/proyectosistdistribuidos/MainActivity.java
+++ app/src/main/java/com/example/proyectosistdistribuidos/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.proyectosistdistribuidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Prueba de Git", Toast.LENGTH_SHORT).show();
+
     }
 }
