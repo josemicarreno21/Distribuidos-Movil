0c1f3d5972fe5f37379c19a337418364054240c4
Sergio <sergio.gabriel.oficial@gmail.com>
2022-05-25 00:04:13 -0400

    Prueba

--- app/src/main/java/com/example/proyectosistdistribuidos/EquiposActivity.java
+++ app/src/main/java/com/example/proyectosistdistribuidos/EquiposActivity.java
@@ -3,12 +3,18 @@ package com.example.proyectosistdistribuidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class EquiposActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipos);
+
+
+
     }
 }
--- app/src/main/java/com/example/proyectosistdistribuidos/MainActivity.java
+++ app/src/main/java/com/example/proyectosistdistribuidos/MainActivity.java
@@ -2,18 +2,41 @@ package com.example.proyectosistdistribuidos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button irEquipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*Toast.makeText(this, "Prueba de Git", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Prueba de Git 2", Toast.LENGTH_SHORT).show();*/
+        irEquipo = findViewById(R.id.irEquipo);
+
+        irEquipo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread background = new Thread(){
+                    public void run(){
+                        try {
+                            sleep(2500);
+                            Intent i = new Intent(getBaseContext(),EquiposActivity.class);
+                            startActivity(i);
+                            finish();
+                        }catch (Exception e){
+                        }
+                    }
+                };
+                background.start();
+
+            }
+        });
+
 
     }
 }
