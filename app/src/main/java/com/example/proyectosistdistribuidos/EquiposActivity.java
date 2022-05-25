cda0600b53b2b486a28d2bfeec8af37255cc78a1
SERGIO <sergio.gabriel.oficial@gmail.com>
2022-05-25 15:28:19 -0400

    Añadido acceso a jugadores

--- app/src/main/java/com/example/proyectosistdistribuidos/EquiposActivity.java
+++ app/src/main/java/com/example/proyectosistdistribuidos/EquiposActivity.java
@@ -2,17 +2,30 @@ package com.example.proyectosistdistribuidos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class EquiposActivity extends AppCompatActivity {
 
-
+    Button irJugador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipos);
+
+        irJugador = findViewById(R.id.irJugador);
+
+        irJugador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent jugador = new Intent(getBaseContext(),JugadorActivity.class);
+                startActivity(jugador);
+
+            }
+        });
 
 
 
