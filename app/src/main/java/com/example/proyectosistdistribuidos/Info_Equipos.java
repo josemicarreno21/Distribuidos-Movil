690433423dba10a4ab3c8e291ba268679bcaf5c5
ClaudiaJH <90428660+ClaudiaJH@users.noreply.github.com>
2022-06-03 15:43:25 -0400

    qfwfq

--- app/src/main/java/com/example/proyectosistdistribuidos/Info_Equipos.java
+++ app/src/main/java/com/example/proyectosistdistribuidos/Info_Equipos.java
@@ -3,12 +3,37 @@ package com.example.proyectosistdistribuidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.proyectosistdistribuidos.clases.Jugadores;
+import com.example.proyectosistdistribuidos.clases.Partidos;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Info_Equipos extends AppCompatActivity {
+    ListView listView;
+    List<Jugadores> jugadoresList;
+    Formacion_Adapter formacion_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_equipos);
+
+        setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.lvPartidos);
+        cargarJugadores();
+        formacion_adapter=new Formacion_Adapter(jugadoresList,R.layout.lista_formacion,this);
+        listView.setAdapter(formacion_adapter);
+    }
+
+    private void cargarJugadores() {
+        jugadoresList = new ArrayList<>();
+        jugadoresList.add(new Jugadores("1","Jose","Carreno Angola", "02-06-2000",
+                "","Delantero","180","1"));
+        jugadoresList.add(new Jugadores("2","Claudia","Jimenez Herera", "02-06-2001",
+                "","Defensa","175","2"));
     }
 }
